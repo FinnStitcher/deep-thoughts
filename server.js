fe15ce21428e8ce643c80504a094287d92262f64
Finn Stitcher <foundfootage@protonmail.com>
2022-09-02 16:39:31 -0700

    initial files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3125;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(require('./routes'));
+
+mongoose.connect('mongodb://127.0.0.1:27017/deep-thoughts');
+
+app.listen(PORT, () => console.log('http://lcoalhost:' + PORT));
